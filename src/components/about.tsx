bb10c05ef55d89d149038df4d2ee46b5d8f20c8b
GautamPanickar <gautam.panickarss@gmail.com>
2017-10-15 12:44:49 +0530

    Added images for homepage about an vision section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -10,14 +10,16 @@ export const About = (props: Props) => {
 	<div>
 		<section className="bg-primary" id="about">
 	        <div className="container">
-	            <div className="row">
-	            	<div className="hover08 column">
-					    <div>
-					        <figure><img src="img/image003.jpg" /></figure>
-					    </div>
-					 </div>
-	                <div className="col-lg-8 col-lg-offset-2 text-center">
-	                    <h2 className="section-heading text-black-dark">{props.resourceFileData.homePage.about.aboutHeader}</h2>
+	            <div className="row">	  
+	                <div className="col-lg-6">
+		                <div className="hover08 column">
+						    <div>
+						        <figure><img src="img/about.jpg" /></figure>
+						    </div>
+						 </div>
+					</div>
+					<div className="col-lg-6 text-left">
+	                    <h2 className="section-heading text-black-dark text-center">{props.resourceFileData.homePage.about.aboutHeader}</h2>
 	                    <hr className="light"></hr>
 	                    <p className="text-faded">{props.resourceFileData.homePage.about.about_para_1}</p>
 	                    <p className="text-faded">{props.resourceFileData.homePage.about.about_para_2}</p>
@@ -26,6 +28,7 @@ export const About = (props: Props) => {
 	                    <a href="#vision" className="page-scroll btn btn-default-dark btn-xl sr-button">
 	                    	{props.resourceFileData.homePage.seOurVision}
 	                    </a>
+	                    
 	                </div>
 	            </div>
 	        </div>
@@ -33,14 +36,21 @@ export const About = (props: Props) => {
 	    <section className="bg-dark" id="vision">
 	        <div className="container">
 	            <div className="row">
-	                <div className="col-lg-8 col-lg-offset-2 text-center">
-	                    <h2 className="section-heading text-white">{props.resourceFileData.homePage.vision.visionHeader}</h2>
+	                <div className="col-lg-6 text-left">
+	                    <h2 className="section-heading text-white text-center">{props.resourceFileData.homePage.vision.visionHeader}</h2>
 	                    <hr className="light"></hr>
 	                    <p className="text-white">{props.resourceFileData.homePage.vision.vision_para_1}</p>
 	                    <p className="text-white">{props.resourceFileData.homePage.vision.vision_para_2}</p>
 	                    <p className="text-white">{props.resourceFileData.homePage.vision.vision_para_3}</p>
 	                    <br></br>
 	                </div>
+	                <div className="col-lg-6">
+		                <div className="hover08 column">
+						    <div>
+						        <figure><img src="img/vision.jpg" /></figure>
+						    </div>
+						 </div>
+					</div>
 	            </div>
 	        </div>
 	    </section>
